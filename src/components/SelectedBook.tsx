493803e1169912d5e82e43213e0023be6ec1f0d9
Rebecca Aaland <reaaland@gmail.com>
2026-08-10 15:23:50 -0500

    chore: use next image for selected book

--- src/components/SelectedBook.tsx
+++ src/components/SelectedBook.tsx
@@ -1,6 +1,7 @@
-import { Book } from "@/types/book";
-import "@/styles/selectedBook.css";
+import Image from "next/image";
 import Link from "next/link";
+import type { Book } from "@/types/book";
+import "@/styles/selectedBook.css";
 
 interface SelectedBookProps {
   book: Book;
@@ -8,35 +9,25 @@ interface SelectedBookProps {
 
 export default function SelectedBook({ book }: SelectedBookProps) {
   return (
-    <Link
-      href={`/book/${book.id}`}
-      className="selected-book__link"
-    >
+    <Link href={`/book/${book.id}`} className="selected-book__link">
       <section className="selected-book">
-        <div className="selected-book__subtitle">
-          {book.subTitle}
-        </div>
+        <div className="selected-book__subtitle">{book.subTitle}</div>
 
         <div className="selected-book__image-wrapper">
-          <img
+          <Image
             className="selected-book__image"
             src={book.imageLink}
             alt={book.title}
+            width={180}
+            height={260}
+            unoptimized
           />
         </div>
 
         <div className="selected-book__info">
-          <h2 className="selected-book__title">
-            {book.title}
-          </h2>
-
-          <p className="selected-book__author">
-            {book.author}
-          </p>
-
-          <p className="selected-book__type">
-            {book.type}
-          </p>
+          <h2 className="selected-book__title">{book.title}</h2>
+          <p className="selected-book__author">{book.author}</p>
+          <p className="selected-book__type">{book.type}</p>
         </div>
       </section>
     </Link>
